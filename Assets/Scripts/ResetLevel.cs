21c2927aefb70550d3c43a8e993fc0c8fe1a9f4f
16Byte <pixeldevs.diego@gmail.com>
2015-08-15 18:24:19 -0600

    Core Files

---
+++ Assets/Scripts/ResetLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetLevel : MonoBehaviour {
+
+    private int level = Application.loadedLevel;
+
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(10, 10, 50, 25), "Reset")) {
+            Application.LoadLevel(level);
+        }
+    }
+
+}
